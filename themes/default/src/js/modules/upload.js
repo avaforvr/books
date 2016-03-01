e8ed6aaba651fae8d43a92a9d7b46d044c15edf3
yywang1 <yywang1@i9i8.com>
2016-03-01 20:26:33 +0800

    上传js

--- themes/default/src/js/modules/upload.js
+++ themes/default/src/js/modules/upload.js
@@ -1,9 +1,12 @@
 require('../lib/jqueryForm');
 require('../mod/textarea').init();
+require('../lib/formCheck');
+var config = require('../mod/config');
 
 var elem = {
     "attaForm": $('#attaForm'),
     "uploadForm": $('#uploadForm'),
+    "editForm": $('#editForm'),
     "sucDone": $('.sucDone'),
     "failDone": $('.failDone'),
 };
@@ -22,7 +25,7 @@ var initAttaForm = function () {
     //选择文件后验证上传到temp目录中
     attaElem.change(function() {
         if(attaElem.val() == '') {
-            attaTip.attr('class', 'tip error').html('请选择文件');
+            attaTip.attr('class', 'tip error').html(config.error + '请选择文件');
             elem.uploadForm.hide();
             return false;
         }
@@ -35,10 +38,10 @@ var initAttaForm = function () {
                     elem.uploadForm.find('input[name="data[book_author]"]').val(r.book_author);
                     elem.uploadForm.find('input[name="data[book_size]"]').val(r.book_size);
                     elem.uploadForm.find('input[name="data[book_path]"]').val(r.book_path);
-                    attaTip.attr('class', 'tip success').html(r.msg);
+                    attaTip.attr('class', 'tip success').html(config.success + r.msg);
                     elem.uploadForm.show();
                 } else {
-                    attaTip.attr('class', 'tip error').html(r.msg);
+                    attaTip.attr('class', 'tip error').html(config.error + r.msg);
                     elem.uploadForm.hide();
                 }
             }
@@ -49,55 +52,50 @@ var initAttaForm = function () {
 
 //提交文件信息表单
 var initUploadForm = function () {
-     //上传和编辑页面表单验证
-    $('#uploadForm, #editForm').submit(function() {
-        var book_type = $('input[name="data[book_type]"]');
-        var btags = $('input[name="data[btags][]"]:checked');
-        var btypeTip = document.getElementById('btypeTip');
-        var btagsTip = document.getElementById('btagsTip');
-        var setTip = function(elemTip, isRight, tipText) {
-            var tipClass = '';
-            if(isRight == 0) {
-                tipClass = 'tip error';
-            } else if(isRight == 1) {
-                tipClass = 'tip success';
-            } else {
-                tipClass = 'tip';
-            }
+    var form = elem.uploadForm.length ? elem.uploadForm : elem.editForm,
+        submitTip = form.find('#submitTip'),
+        submitBtn = form.find('button[type="submit"]');
 
-            elemTip.className = tipClass;
-            elemTip.innerHTML = tipText;
+    var items = {
+        "data[book_name]": [
+            {type: "null", errMsg: '请刷新后重新上传' }
+        ],
+        "data[book_author]": [
+            {type: "null", errMsg: '请刷新后重新上传'}
+        ],
+        "data[book_type]": [
+            {type: "select", value: 0, errMsg: '请选择分类'}
+        ],
+        "check_book_tags": [
+            {type: "book_tags", errMsg: '标签不能超过5个'}
+        ]
+    };
+
+    form.submit(function() {
+        var isChecked = form.formCheck(items, {
+            showSuccess: function (obj, errMsg) {
+                if(obj.attr('name') != 'check_book_tags') {
+                    $(obj).closest('tr').find('.tip').attr('class', 'tip success').html(config.success);
+                }
+            },
+            showError: function (obj, errMsg) {
+                if(obj.attr('name') == 'check_book_tags') {
+                    $('#bookTagsTip').attr('class', 'tip error').html(config.error + errMsg);
+                } else {
+                    $(obj).closest('tr').find('.tip').attr('class', 'tip error').html(config.error + errMsg);
+                }
+            },
+            rules : {'book_tags': function (obj, checks) {
+                return form.find('input[name="check_book_tags[]"]:checked').length <= 5;
+            }}
+        });
+
+        if(isChecked) {
+            submitTip.html(config.loading);
+            submitBtn.prop('disabled', true);
         }
 
-        if(! book_name.val()) {
-            setTip(bnameTip, 0, '请填写书名');
-            book_name.focus(function() {
-                setTip(bnameTip, 2, '');
-            })
-            return false;
-        }
-        if(! book_author.val()) {
-            setTip(bauthorTip, 0, '请填写作者');
-            book_author.focus(function() {
-                setTip(bauthorTip, 2, '');
-            });
-            return false;
-        }
-        if(book_type.val() == 0) {
-            setTip(btypeTip, 0, '请选择分类');
-            book_type.next().mousedown(function() {
-                setTip(btypeTip, 2, '');
-            });
-            return false;
-        }
-        if(btags.length > 5) {
-            setTip(btagsTip, 0, '标签不能超过5个');
-            btags.click(function() {
-                setTip(btagsTip, 2, '');
-            });
-            return false;
-        }
-        return true;
+        return isChecked;
     });
 };
 
