58a6c83e4954724be86e31c3264e3876000a7f43
yywang1 <yywang1@i9i8.com>
2016-03-08 15:21:55 +0800

    master目录整理

--- master/batchUpload.php
+++
@@ -1,9 +0,0 @@
-<?
-include_once __DIR__ . '../../includes/init/global.php';
-$container['util']->checkLogin();
-
-$tplArray['html_main'] = $container['twig']->render('master/batchUpload.html', array());
-
-echo $container['twig']->render('master/c_master_tpl.html', $tplArray);
-
-?>
--- master/report.php
+++ master/report.php
@@ -2,8 +2,5 @@
 include_once __DIR__ . '../../includes/init/global.php';
 $container['util']->checkLogin();
 
-$tplArray['html_main'] = $container['twig']->render('master/report.html', array());
-
-echo $container['twig']->render('master/c_master_tpl.html', $tplArray);
-
+echo $container['twig']->render('master/report.html');
 ?>
