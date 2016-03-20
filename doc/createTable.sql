80b3a5318f05f28de512bd9436719036046f2d80
avaforvr <avaforvr@163.com>
2016-03-20 20:11:47 +0800

    book表添加性向field

--- doc/createTable.sql
+++ doc/createTable.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS book(
 	book_author varchar(100) comment '作者',
 	book_summary text comment '简介',
 	book_size int comment '文件体积',
+	book_sex int(2) comment '类型',
 	book_type int(2) comment '类型',
 	book_style int(2) comment '文风',
 	book_tags varchar(20) comment '标签',
@@ -57,8 +58,8 @@ CREATE TABLE IF NOT EXISTS config(
 ) DEFAULT CHARSET=utf8;
 INSERT INTO config(config_name, config_value)
 VALUES
-	('attr_type', '[{"id": 0, "value": "-"},{"id": 1, "value": "言情"},{"id": 2, "value": "耽美"},{"id": 3, "value": "玄幻武侠"},{"id": 4, "value": "魔法斗气"},{"id": 5, "value": "都市异能"},{"id": 6, "value": "种田生活"},{"id": 7, "value": "历史军事"},{"id": 8, "value": "竞技游戏"},{"id": 9, "value": "未来星际"}]'),
+	('attr_sex', '[{"id": 0, "value": "-"},{"id": 1, "value": "异性CP"},{"id": 2, "value": "同性CP"},{"id": 3, "value": "无CP"}]'),
+	('attr_type', '[{"id": 0, "value": "-"},{"id": 1, "value": "爱情"},{"id": 2, "value": "同人"},{"id": 3, "value": "玄幻武侠"},{"id": 4, "value": "魔法斗气"},{"id": 5, "value": "都市异能"},{"id": 6, "value": "种田生活"},{"id": 7, "value": "历史军事"},{"id": 8, "value": "竞技游戏"},{"id": 9, "value": "未来星际"}]'),
 	('attr_style', '[{"id": 0, "value": "-"},{"id": 1, "value": "轻松"},{"id": 2, "value": "爆笑"},{"id": 3, "value": "甜文"},{"id": 4, "value": "正剧"},{"id": 5, "value": "微虐"},{"id": 6, "value": "很虐"},{"id": 7, "value": "悲剧"}]'),
-	('attr_tags', '[{"id": 1, "value": "同人"},{"id": 2, "value": "穿越重生"},{"id": 3, "value": "异世"},{"id": 4, "value": "未来"},{"id": 5, "value": "末世"},{"id": 6, "value": "清穿"},{"id": 7, "value": "空间"},{"id": 8, "value": "机甲"},{"id": 9, "value": "修真"},{"id": 10, "value": "异能"},{"id": 11, "value": "魔法"},{"id": 12, "value": "种田"},{"id": 13, "value": "明星"},{"id": 14, "value": "高干"},{"id": 15, "value": "生子"},{"id": 16, "value": "反琼瑶"},{"id": 17, "value": "HP"},{"id": 18, "value": "红楼"},{"id": 19, "value": "猎人"},{"id": 20, "value": "火影"},{"id": 21, "value": "死神"},{"id": 22, "value": "网王"},{"id": 23, "value": "武侠"},{"id": 24, "value": "综漫"},{"id": 25, "value": "女尊"},{"id": 26, "value": "NP"}]'),
-	('attr_eval', '[{"id": 0, "value": "-"},{"id": 1, "value": "烂文"},{"id": 2, "value": "一般"},{"id": 3, "value": "经典"}]'),
+	('attr_tags', '[{"id": 1,"value": "穿越重生"},{"id": 2,"value": "异世"},{"id": 3,"value": "未来"},{"id": 4,"value": "末世"},{"id": 5,"value": "空间"},{"id": 6,"value": "机甲"},{"id": 7,"value": "修真"},{"id": 8,"value": "异能"},{"id": 9,"value": "魔法"},{"id": 10,"value": "种田"},{"id": 11,"value": "明星"},{"id": 12,"value": "高干"},{"id": 13,"value": "反琼瑶"},{"id": 14,"value": "HP"},{"id": 15,"value": "红楼"},{"id": 16,"value": "猎人"},{"id": 17,"value": "火影"},{"id": 18,"value": "死神"},{"id": 19,"value": "网王"},{"id": 20,"value": "武侠"},{"id": 21,"value": "综漫"}]'),
 	('attr_status', '[{"id": 0, "value": "待删除"},{"id": 1, "value": "通过"},{"id": 2, "value": "待审核"},{"id": 3, "value": "侵权"}]');
